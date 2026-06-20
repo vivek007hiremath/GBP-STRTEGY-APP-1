8f1bf20abd06f657373c3ddce9326c86cde248e7
vivek007hiremath <vivek007hiremath@gmail.com>
2026-06-20 14:33:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/gbpusd-backtest/',
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom'],
-          'charts': ['recharts'],
-        },
-      },
-    },
-  },
 })
